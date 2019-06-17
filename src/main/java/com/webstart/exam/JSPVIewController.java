7da11cda92f98d3d7dd1af7d7fb32d5432f64bbd
kghx2 <kghx2@winitech.com>
2019-06-17 16:09:41 +0900

    comment

--- src/main/java/com/webstart/exam/JSPVIewController.java
+++ src/main/java/com/webstart/exam/JSPVIewController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JSPVIewController {
+public class JSPVIewController { 
 	@RequestMapping(value="/")  
 	public String root(HttpServletRequest request) {
         HttpSession session = request.getSession();
